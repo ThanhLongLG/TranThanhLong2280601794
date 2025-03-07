07d1ed4c0ca588f7c2875338f009228228f796cf
ThanhLongLG <ttlong24092le4@gmail.com>
2025-03-07 11:19:23 +0700

    hoan thanh

--- lab02/ex01/api.py
+++ lab02/ex01/api.py
@@ -19,6 +19,25 @@ def caesar_decrypt():
     key = int(data['key'])
     decrypted_text=caesar_cipher.decrypt_text(cipher_text,key)
     return jsonify({'decrypted_message': decrypted_text})
+from cipher.vigenere import VigenereCipher
+vigenere_cipher = VigenereCipher()
+
+@app.route('/api/vigenere/encrypt',methods=['POST'])
+def vigenere_encrypt():
+    data=request.json
+    plain_text=data['plain_text']
+    key = int(data['key'])
+    encrypted_text=vigenere_cipher.vigenere_encreypt(plain_text,key)
+    return jsonify({'encrypted_message': encrypted_text})
+
+
+@app.route("/api/vigenere/decrypt",methods=["POST"])
+def vigenere_decrypt():
+    data=request.json
+    cipher_text=data['cipher_text']
+    key = int(data['key'])
+    decrypted_text=vigenere_cipher.vigenere_decrypt(cipher_text,key)
+    return jsonify({'decrypted_message': decrypted_text})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
