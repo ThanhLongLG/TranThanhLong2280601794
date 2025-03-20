20da492d126017fbccd5197df6b49229d212bcef
ThanhLongLG <ttlong24092le4@gmail.com>
2025-03-20 17:11:27 +0700

    hoanthanhbai2

--- lab02/api.py
+++ lab02/api.py
@@ -3,12 +3,31 @@ from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
 from cipher.railfence import RailFenceCipher
 from cipher.playfair import PlayFairCipher
+from cipher.transposition import TranspositionCipher
 app = Flask(__name__)
 
+transposition_cipher = TranspositionCipher()
 caesar_cipher = CaesarCipher()
 vigenere_cipher =VigenereCipher()
 railfence_cipher =RailFenceCipher()
 playfair_cipher = PlayFairCipher()
+
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+@app.route('/api/vigenere/encrypt', methods=['POST'])
 
 @app.route("/api/playfair/creatematrix",methods=["POST"])
 def playfair_creatematrix():
