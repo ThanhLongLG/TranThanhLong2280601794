d13cad1a3968f6c76830be5e4f084a4f7a424ac9
unknown <ttlong24092le4@gmail.com>
2025-03-21 10:58:04 +0700

    hoan thanh bai

--- Lab04/dh_key_pair/client.py
+++ Lab04/dh_key_pair/client.py
@@ -11,18 +11,17 @@ def derive_shared_key(private_key, public_key):
     return shared_key
 
 def main():
-    # Tải khóa công khai của server
+
     with open("server_public_key.pem", "rb") as f:
         server_public_key = serialization.load_pem_public_key(f.read())
     
-    # Tải tham số DH từ server
+
     with open("server_parameters.pem", "rb") as f:
         parameters = serialization.load_pem_parameters(f.read())
     
-    # Tạo cặp khóa của client dựa trên tham số của server
+
     private_key, public_key = generate_client_key_parameters(parameters)
     
-    # Dẫn xuất khóa chung
     shared_secret = derive_shared_key(private_key, server_public_key)
     
     print("Shared secret:", shared_secret.hex())
